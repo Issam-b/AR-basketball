d405f9b397ac34c9564bef282e780932095d45dc
Issam <assam.bj@gmail.com>
2017-10-11 22:22:52 +0300

    Some modificatins
    TODO: modify the basket hoop model, use box instead of plane.

---
+++ UCF-basket-AR/Assets/Scripts/BallController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour {
+
+    public GameObject ball;
+    //public float smoothSpeed = 0.125f;
+    public Vector3 offset = new Vector3(0, -3, 4.5f);
+
+    private void LateUpdate()
+    {
+        ball.transform.position = transform.position + offset;
+    }
+}
