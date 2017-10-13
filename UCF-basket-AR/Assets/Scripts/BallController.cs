38d93a266cbeca230f6e0a0c9e3a67a93f1d1d0e
Issam <assam.bj@gmail.com>
2017-10-14 00:28:03 +0300

    project changes, activating camera tracking and post processing

--- UCF-basket-AR/Assets/Scripts/BallController.cs
+++ UCF-basket-AR/Assets/Scripts/BallController.cs
@@ -5,15 +5,14 @@ using UnityEngine;
 public class BallController : MonoBehaviour {
 
     public GameObject ball;
-    //public float smoothSpeed = 0.125f;
-    public Vector3 offset = new Vector3(0f, -3, +100);
+    public Vector3 offset;
 
     private void Start()
     {
-    //    ball.transform.position = this.transform.position + offset;
+
     }
     void Update()
     {
-        ball.transform.position = transform.position + new Vector3(x: 0f, y: -2.1f, z: 6.13f);
+        ball.transform.position = transform.position + offset;
     }
 }
