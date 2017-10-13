cabd66709bfb346763b2fe8c6a47025d650d604e
Issam <assam.bj@gmail.com>
2017-10-13 20:36:56 +0300

    nothing much

--- UCF-basket-AR/Assets/Scripts/BallController.cs
+++ UCF-basket-AR/Assets/Scripts/BallController.cs
@@ -6,10 +6,14 @@ public class BallController : MonoBehaviour {
 
     public GameObject ball;
     //public float smoothSpeed = 0.125f;
-    public Vector3 offset = new Vector3(0, -3, 4.5f);
+    public Vector3 offset = new Vector3(0f, -3, +100);
 
+    private void Start()
+    {
+    //    ball.transform.position = this.transform.position + offset;
+    }
     void Update()
     {
-        ball.transform.position = transform.position - offset;
+        ball.transform.position = transform.position + new Vector3(x: 0f, y: -2.1f, z: 6.13f);
     }
 }
